a35c413b54acf0927ac6870d8b1dd36b87b14e82
zarmstrong <zarmstrong@traffid.com>
2021-07-11 18:01:14 -0600

    update module.json for 0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# hide-empty-spells
-A  Foundry VTT module to hide empty spell sections in PF2e
+[![ko-fi](https://img.shields.io/badge/-buy%20me%20a%20coffee-%23FF5E5B?style=plastic)](https://ko-fi.com/slate) [![patreon](https://img.shields.io/badge/-support%20me%20on%20patreon-%235C5C5C?style=plastic)](https://patreon.com/slatesfoundrystuff) ![GitHub release (by tag)](https://img.shields.io/github/downloads/zarmstrong/hide-empty-spells/hide-empty-spells-0.1.1/total?style=plastic) ![GitHub all releases](https://img.shields.io/github/downloads/zarmstrong/hide-empty-spells/total?style=plastic) ![GitHub](https://img.shields.io/github/license/zarmstrong/hide-empty-spells?style=plastic)
+
+# Hide Empty Spells
+
+## Summary:
+A  Foundry VTT module to hide empty spell sections in PF2e.  
+
+**PF2e system version 2.9.1 or later is required.**
--- hide-empty-spells.js
+++ hide-empty-spells.js
@@ -1,22 +1,44 @@
 const debugHE = true;
 let hideEmpty = false;
 // message is the error message, level is the error level: 0 = info, 1 = warn, 2 = error
-function log(message, level) {
+function log(message, level=0) {
 	const logColor1 = "color: #7bf542"; //bright green
 	const logColor2 = "color: #d8eb34"; //yellow green
-	const logColor3 = "color: #ffffff"; //white
+	const logColor3 = "color: #085e80"; //blue
 	const logColor4 = "color: #cccccc"; //gray
-	// const logColor5 = "color: #cccccc"; //gray
+	const logColor5 = "color: #801008"; //red
 	let color1, color2;
 
-	if (level == 2)
+	if (level == 2) {
+		color1=logColor1;
+		color2=logColor5;
 		if (debugHE)
 			console.log(
-				"%cHeroVau.lt/Foundry Bridge | %ccheckUserToken: " +
-					JSON.stringify(responseJSON),
-				hvColor1,
-				hvColor4
+				`%cHide Empty Spells | %c `,
+				color1,
+				color2,
+				message
 			);
+	} else if (level == 1) {
+		color1=logColor1;
+		color2=logColor3;
+		if (debugHE)
+			console.log(
+				"%cHide Empty Spells | %c " ,
+				color1,
+				color2,
+				message
+			);
+	} else {
+		color1=logColor1;
+		color2=logColor4;
+			console.log(
+				"%cHide Empty Spells | %c " ,
+				color1,
+				color2,
+				message
+			);
+	}
 }
 
 Hooks.on("ready", async function () {
@@ -33,13 +55,13 @@ Hooks.on("renderActorSheet", function (obj, html) {
 	const actor = obj.actor;
 	if (!(actor.data.type === "character")) return;
 	hideEmpty = game.settings.get("hide-empty-spells", "hideEmpty");
-	console.log("hideEmpty: " + hideEmpty);
+	log("hideEmpty: " + hideEmpty,2);
 	if (hideEmpty == 'true') {
-		console.log("hiding all")
+		log("hiding all",2)
 		addButton(html);
 		hideEmptySpells(html);
 	} else {
-		console.log("showing all")
+		log("showing all")
 		addButton(html, 0);
 		showEmptySpells(html);
 	}
@@ -54,10 +76,9 @@ function addButton(html, hide = 1) {
 
 	sheetID = $(html).attr("id");
 	let spellsPage = html.find(`[data-tab='spellbook']`);
-	let button = $(spellsPage).find(`ol.singleColumn-list`);
-
+	let button = $(spellsPage).find(`ol.spellcastingEntry-list.directory-list.item-list`);
 	let newButton =
-		'<li class="item inventory-header spellbook-header spellbook-empty" draggable="true"><div class="item-controls pf-bluelist pf-add-item-row"><a class="item-control spellcasting-create" id="showspells" title="' +
+		'<li class="item inventory-header spellbook-header spellbook-empty" id="showspells" draggable="true"><div class="item-controls pf-bluelist pf-add-item-row"><a class="item-control spellcasting-create" title="' +
 		hidetitle +
 		'" ><i class="fas fa-' +
 		hidefa +
@@ -66,7 +87,7 @@ function addButton(html, hide = 1) {
 		"</a></div></li>";
 	$(button).append(newButton);
 	newButton =
-		'<li class="item inventory-header spellbook-header spellbook-empty" draggable="true"><div class="item-controls pf-bluelist pf-add-item-row"><a class="item-control spellcasting-create" id="hidespells" title="' +
+		'<li class="item inventory-header spellbook-header spellbook-empty" id="hidespells" draggable="true"><div class="item-controls pf-bluelist pf-add-item-row"><a class="item-control spellcasting-create" title="' +
 		showtitle +
 		'" ><i class="fas fa-' +
 		showfa +
@@ -95,28 +116,30 @@ function addButton(html, hide = 1) {
 
 function hideEmptySpells(html) {
 	let spellsPage = html.find(`[data-tab='spellbook']`);
-	// console.log(spellsPage);
+	// log(spellsPage);
 	let spellContainers = spellsPage.find(
 		`[data-container-type="spellcastingEntry"]`
 	);
 
 	for (let x = 0; x < spellContainers.length; x++) {
 		let spellBox = spellContainers[x];
-		// console.log($(spellLevel).find('ol'))
+		log(spellBox,2);
 		let spellcastingEntry = $(spellBox).find(
-			"ol.inventory-list.directory-list"
+			"ol.directory-list.item-list.spell-list"
 		);
-
+		log(spellcastingEntry,2)
 		for (let y = 0; y < spellcastingEntry.length; y++) {
 			let spellLevel = $(spellcastingEntry).find(
 				"li.item.inventory-header.spellbook-header"
 			);
+			log(spellLevel,2);
 			for (let z = 0; z < spellLevel.length; z++) {
 				let numSpells = $(spellLevel[z])
 					.find("span.spell-max-input input")
 					.val();
 
 				if (numSpells == "0") {
+					log(spellLevel[z],2);
 					$(spellLevel[z]).hide();
 					//hide other related ones:
 					$(spellLevel[z])
@@ -137,7 +160,7 @@ let spellContainers = spellsPage.find(
 for (let x = 0; x < spellContainers.length; x++) {
 	let spellBox = spellContainers[x];
 	let spellcastingEntry = $(spellBox).find(
-		"ol.inventory-list.directory-list"
+		"ol.directory-list.item-list.spell-list"
 	);
 
 	for (let y = 0; y < spellcastingEntry.length; y++) {
